9cdf1727863e4bd542f6015c825888540710d168
Ezekiel <theatlasroom@gmail.com>
2023-05-26 15:52:20 +1000

    [skip ci] wip

--- cmd/lbxd.go
+++ cmd/lbxd.go
@@ -8,6 +8,16 @@ import (
 	"github.com/gocolly/colly"
 )
 
+type WatchlistPoster struct {
+	ID              int    `csv:"ID"`
+	Name            string `csv:"Name"`
+	Slug            string `csv:"Slug"`
+	Height          int    `csv:"Height"`
+	Width           int    `csv:"Width"`
+	CacheBustingKey string `csv:"CacheBustingKey"`
+	MetadataURL     string `csv:"MetadataURL` // should only ever need to construct this once for a new movie
+}
+
 type LetterboxdMovie struct {
 	ID          int       `csv:"ID"`
 	Name        string    `csv:"Name"`
@@ -16,6 +26,7 @@ type LetterboxdMovie struct {
 	URI         string    `csv:"Letterboxd URI"`
 }
 
+const lbxdBaseURL = "https://letterboxd.com"
 const watchlistBaseURL = "https://letterboxd.com/junior1z1337/watchlist"
 
 const (
@@ -29,50 +40,81 @@ const (
 )
 
 const (
-	SELECTOR_FILM_NAME         = "data-film-name"
-	SELECTOR_FILM_ID           = "data-film-id"
-	SELECTOR_FILM_SLUG         = "data-film-slug"
-	SELECTOR_FILM_RELEASE_YEAR = "data-film-release-year"
+	SELECTOR_FILM_NAME                = "data-film-name"
+	SELECTOR_FILM_ID                  = "data-film-id"
+	SELECTOR_FILM_SLUG                = "data-film-slug"
+	SELECTOR_FILM_RELEASE_YEAR        = "data-film-release-year"
+	SELECTOR_POSTER_HEIGHT            = "data-image-height"
+	SELECTOR_POSTER_WIDTH             = "data-image-width"
+	SELECTOR_POSTER_CACHE_BUSTING_KEY = "data-cache-busting-key"
 )
 
 func extractPoster(e *colly.HTMLElement) {}
+func onScraped()                         {} // called at the end of the whole scraping process
 func onPageHTML(e *colly.HTMLElement)    {}
 func onRequest(r *colly.Request)         {}
 
+func onResponse(r *colly.Response) {
+	fmt.Println("Response: ", r.Request.URL)
+	fmt.Println("Response: ", string(r.Body))
+}
+
+func onError() {}
+
+func metadataURL(p *WatchlistPoster) string {
+	url := "/ajax/poster%sstd/%dx%d?k=%s"
+	return fmt.Sprintf(url, p.Slug, p.Width, p.Height, p.CacheBustingKey)
+}
+
+func AttrInt(str string) int {
+	val, strErr := strconv.Atoi(str)
+	if strErr != nil {
+		return -1
+	}
+	return val
+}
+
+func NewWatchlistPoster(el *colly.HTMLElement) *WatchlistPoster {
+	p := &WatchlistPoster{
+		ID:              AttrInt(el.Attr(SELECTOR_FILM_ID)),
+		Name:            el.DOM.Find("img").AttrOr("alt", ""),
+		Slug:            el.Attr(SELECTOR_FILM_SLUG),
+		Height:          AttrInt(el.Attr(SELECTOR_POSTER_HEIGHT)),
+		Width:           AttrInt(el.Attr(SELECTOR_POSTER_WIDTH)),
+		CacheBustingKey: el.Attr(SELECTOR_POSTER_CACHE_BUSTING_KEY),
+	}
+	p.MetadataURL = metadataURL(p)
+
+	return p
+}
+
 func main() {
-	posters := []LetterboxdMovie{}
+	watchlistPosters := []WatchlistPoster{}
 
 	collector := colly.NewCollector(
 		colly.CacheDir("./tmp"),
 	)
+
+	// collector.Async = true
 	// paginationCollector := collector.Clone()
 
 	collector.OnHTML(NODE_POSTER_CONTAINER, func(e *colly.HTMLElement) {
 		e.ForEach(NODE_POSTER, func(_ int, el *colly.HTMLElement) {
 
-			ID, iderr := strconv.Atoi(el.Attr(SELECTOR_FILM_ID))
+			// ReleaseYear, ryerr := strconv.Atoi(el.Attr(SELECTOR_FILM_RELEASE_YEAR))
+			// if ryerr != nil {
+			// 	ReleaseYear = 0
+			// }
 
-			if iderr != nil {
-				ID = -1
-			}
-
-			ReleaseYear, ryerr := strconv.Atoi(el.Attr(SELECTOR_FILM_RELEASE_YEAR))
-			if ryerr != nil {
-				ReleaseYear = 0
-			}
-
-			p := LetterboxdMovie{
-				ID:          ID,
-				ReleaseYear: int16(ReleaseYear),
-				Name:        el.DOM.Find("img").AttrOr("alt", ""),
-				URI:         el.Attr(SELECTOR_FILM_SLUG),
-			}
+			p := NewWatchlistPoster(el)
 
 			fmt.Println("Found a poster", p)
+			// fmt.Sprintf("Found a poster %+v, %s", p, metadataURL(p))
 			// fire up a filmCollector
 			// for each slug, we should visit the page and extract the metadata
 
-			posters = append(posters, p)
+			e.Request.Visit(p.MetadataURL)
+			watchlistPosters = append(watchlistPosters, *p)
 		})
 	})
 
@@ -118,9 +160,16 @@ func main() {
 		// e.Request.Visit(nextPage)
 	})
 
+	collector.OnResponse(onResponse)
+
 	// paginationCollector.OnHTML(NODE_PAGINATION_PAGE, func(e *colly.HTMLElement) {
 	// 	nextURL := e.Attr("href")
 	// })
+
+	// Set error handler
+	collector.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
 
 	collector.OnRequest(func(r *colly.Request) {
 		fmt.Println("Requesting ", r.URL)
